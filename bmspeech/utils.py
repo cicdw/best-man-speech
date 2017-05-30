2b1420feaf3cb29a1aa3369a83cddbbde173062a
Chris White <white.cdw@gmail.com>
2017-05-30 17:39:21 -0400

    Update utils and such

--- bmspeech/utils.py
+++ bmspeech/utils.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 
+def print_top(vector, mapper=None, n=10, display_vals=False):
+    ranks = list(reversed(np.argsort(vector)))
+    for rank, idx in enumerate(ranks[:n]):
+        values = ': {}'.format(vector[idx]) if display_vals else ''
+        print('{0}.) {1}'.format(rank + 1, mapper[idx]) + values)
+
+
 def create_local_vec(keys, mapper):
     keys = [keys] if isinstance(keys, str) else keys
     lv = np.zeros(len(mapper))
@@ -9,3 +16,14 @@ def create_local_vec(keys, mapper):
         if i is not None:
             lv[mapper[key]] = 1
     return lv / lv.sum()
+
+
+def generate_string_from_weights(eig, worker, stop_words={}):
+    fake_counts = {}
+    fake_vals = (eig * 1e3).round().tolist()
+    for idx, val in enumerate(fake_vals):
+        word = worker[idx]
+        if word in stop_words:
+            pass
+        else:
+            fake_counts[word] = max(val, 1)
