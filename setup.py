8b6251253d79dc3fa37f8e349174cba0f9407e7a
Chris White <white.cdw@gmail.com>
2017-05-30 15:40:26 -0400

    Throw together environment file and some example images.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='bmspeech',
+    version='0.0.1',
+    description='Quick-and-dirty python module for finding word associations in emails.',
+    url='https://github.com/moody-marlin/best-man-speech',
+    author='Chris White',
+    author_email='white.cdw@gmail.com'
+)
